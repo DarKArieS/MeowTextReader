2f5b0b406791540eeb76d5caf9561923431712c2
MeowChiaun <cwyeh@91app.com>
2025-08-15 17:52:13 +0800

    clean import, adjust main window layout

--- MeowTextReader/MainWindow.xaml.cs
+++ MeowTextReader/MainWindow.xaml.cs
@@ -1,20 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.Storage.Pickers;
 using Windows.Storage;
-using System.Threading.Tasks;
+using Windows.Storage.Pickers;
 
 namespace MeowTextReader
 {
