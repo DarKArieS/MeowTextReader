f8e970a9160837f2d1e865d49aeebf839b28896b
MeowChiaun <cwyeh@91app.com>
2025-08-15 16:44:24 +0800

    impl dir click and back

--- MeowTextReader/MainViewModel.cs
+++ MeowTextReader/MainViewModel.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using System.Windows.Input;
 
 namespace MeowTextReader
 {
@@ -31,10 +32,35 @@ namespace MeowTextReader
 
         public ObservableCollection<FileItem> FolderItems { get; } = new();
 
+        public ICommand FolderItemClickCommand { get; }
+        public ICommand BackCommand { get; }
+
         public MainViewModel()
         {
+            FolderItemClickCommand = new RelayCommand<FileItem>(OnFolderItemClick);
+            BackCommand = new RelayCommand(BackToParent);
             OnPropertyChanged(nameof(FolderPath));
             LoadFolderItems();
+        }
+
+        private void OnFolderItemClick(FileItem? item)
+        {
+            if (item != null && item.IsFolder && !string.IsNullOrEmpty(FolderPath))
+            {
+                FolderPath = Path.Combine(FolderPath, item.Name);
+            }
+        }
+
+        private void BackToParent()
+        {
+            if (!string.IsNullOrEmpty(FolderPath))
+            {
+                var parent = Directory.GetParent(FolderPath);
+                if (parent != null)
+                {
+                    FolderPath = parent.FullName;
+                }
+            }
         }
 
         private void LoadFolderItems()
@@ -56,5 +82,35 @@ namespace MeowTextReader
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
+    }
+
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool>? _canExecute;
+        public RelayCommand(Action execute, Func<bool>? canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+        public bool CanExecute(object? parameter) => _canExecute?.Invoke() ?? true;
+        public void Execute(object? parameter) => _execute();
+        public event EventHandler? CanExecuteChanged;
+        public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T?> _execute;
+        private readonly Func<T?, bool>? _canExecute;
+        public RelayCommand(Action<T?> execute, Func<T?, bool>? canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+        public bool CanExecute(object? parameter) => _canExecute?.Invoke((T?)parameter) ?? true;
+        public void Execute(object? parameter) => _execute((T?)parameter);
+        public event EventHandler? CanExecuteChanged;
+        public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
     }
 }
