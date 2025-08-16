5b5f4043cb28d750e195885136cb5ed177e353d2
DarKArieS <dk.aries10@gmail.com>
2025-08-16 20:44:52 +0800

    feat: optimize font size setting ui

--- MeowTextReader/ReaderPage/SettingsDialog.xaml.cs
+++ MeowTextReader/ReaderPage/SettingsDialog.xaml.cs
@@ -1,4 +1,6 @@
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Input;
+using System;
 
 namespace MeowTextReader.ReaderPage
 {
@@ -8,6 +10,38 @@ namespace MeowTextReader.ReaderPage
         {
             this.InitializeComponent();
             this.DataContext = new SettingsDialogViewModel();
+        }
+
+        private void DecreaseFontSize_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+        {
+            if (DataContext is SettingsDialogViewModel vm)
+            {
+                if (vm.FontSize > 1)
+                    vm.FontSize -= 1;
+            }
+        }
+
+        private void IncreaseFontSize_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+        {
+            if (DataContext is SettingsDialogViewModel vm)
+            {
+                vm.FontSize += 1;
+            }
+        }
+
+        private void FontSizeTextBox_PreviewKeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            // 只允許數字、刪除、方向鍵
+            if (!(e.Key >= Windows.System.VirtualKey.Number0 && e.Key <= Windows.System.VirtualKey.Number9) &&
+                !(e.Key >= Windows.System.VirtualKey.NumberPad0 && e.Key <= Windows.System.VirtualKey.NumberPad9) &&
+                e.Key != Windows.System.VirtualKey.Back &&
+                e.Key != Windows.System.VirtualKey.Delete &&
+                e.Key != Windows.System.VirtualKey.Left &&
+                e.Key != Windows.System.VirtualKey.Right &&
+                e.Key != Windows.System.VirtualKey.Tab)
+            {
+                e.Handled = true;
+            }
         }
     }
 }
